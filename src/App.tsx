b93106b303c326290e0708177aaf03370650a05a
Cekka <francy4e2009@hotmail.it>
2024-06-21 18:24:02 +0200

    feat: add Tailwind CSS and NextUI packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
+import { Link, Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/react"
 
 export const App = () => {
 
   return (
     <>
+      <Navbar shouldHideOnScroll>
+        <NavbarBrand>
+          <p className="font-bold text-inherit">DISC GOLF</p>
+        </NavbarBrand>
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              Home
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#" aria-current="page">
+              Frisbee
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              How to play
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              Points
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              Find fields
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import {App} from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.ts'
+import './index.css'
+import { NextUIProvider } from '@nextui-org/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
     </Provider>
   </React.StrictMode>,
 )
